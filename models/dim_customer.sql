eaf0bcdd36c34c7df7b96b82ca69ab0356adafcb
Dat Nguyen <dat@infinitelambda>
2024-03-05 16:51:56 +0700

    feat: add unit test

---
+++ models/dim_customer.sql
@@ -0,0 +1,18 @@
+with customers as (
+
+    select * from {{ ref('stg_customers') }}
+
+),
+    
+check_valid_emails as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customers.last_name = customers.first_name as is_valid_customer_name
+    from customers
+
+)
+
+select * from check_valid_emails
